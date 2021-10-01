24218441113d244c944520f56e148f205a7810fa
Ivanov Daniil <90034952+DaniilIvanov4@users.noreply.github.com>
2021-10-01 12:34:52 +0300

    Add files via upload

---
+++ Python1'.py
@@ -0,0 +1,30 @@
+'''
+x = input('')
+if x:
+    print('Салам')'''
+
+'''
+x = float(input('Введите длину '))
+y = float(input('Введите ширину '))
+print('Площадь равна', x*y)'''
+
+'''
+x = int(input())
+print(sum(range(1,x+1)))'''
+
+
+'''
+x = int(input("Введи часы "))
+y = int(input("Введи минуты "))
+z = int(input("Введи секунды "))
+print(x*3600+y*60+z)'''
+
+'''
+x = float(input("Введи рост "))
+y = float(input("Введи вес "))
+print("Индекс массы тела равен ",(y / (x**2)))'''
+
+
+'''
+x = int(input())
+print(sum(map(int, list(str(x)))))'''
